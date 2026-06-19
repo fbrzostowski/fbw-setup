82d1e903355a45cad4a83490956f1d4506e6b328
fbrzostowski <brzostowski.f@gmail.com>
2026-06-19 15:30:11 +0200

    Update install-fbw.sh

--- install-fbw.sh
+++ install-fbw.sh
@@ -1,12 +1,12 @@
 #!/bin/zsh
-echo "FBW - Firmowa Baza Wiedzy"
-echo "Prototyp"
-echo "================"
+curl -sL https://raw.githubusercontent.com/fbrzostowski/fbw-setup/main/ascii-art.txt
 echo "Czy chcesz dodac alias?"
 echo -n "[y/n]: "
 read continue < /dev/tty
 if [ "$continue" = "y" ]; then
   echo 'alias fbw="ssh demo@192.168.1.137"' >> ~/.zshrc
+  clear
+  echo "Dodano alias fwb:FIRMOWA BAZA WIEDZY"
   echo "Wpisz 'source ~/.zshrc' lub zrestartuj terminal"
 else
   echo "Przerywam..."
