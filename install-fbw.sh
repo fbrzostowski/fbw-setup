b2294c3d491797eeabbf5463af034f844a55337a
fbrzostowski <brzostowski.f@gmail.com>
2026-06-19 15:03:59 +0200

    Fix read command to read from /dev/tty

--- install-fbw.sh
+++ install-fbw.sh
@@ -4,7 +4,7 @@ echo "Prototyp"
 echo "================"
 echo "Czy chcesz dodac alias?"
 echo -n "[y/n]: "
-read continue
+read continue < /dev/tty
 if [ "$continue" = "y" ]; then
   echo 'alias fbw="ssh demo@192.168.1.137"' >> ~/.zshrc
   echo "Wpisz 'source ~/.zshrc' lub zrestartuj terminal"
